d54582998b7d746aeb042623d516e1a056fa30d4
Grant Sander <gksander93@gmail.com>
2020-10-16 07:26:39 -0500

    Use some scss, TS setup, sample content container changes

--- src/components/Samples/LoadingBlocks.tsx
+++ src/components/Samples/LoadingBlocks.tsx
@@ -1,9 +1,8 @@
 import * as React from "react";
-import { motion } from "framer-motion";
+import styles from "./LoadingBlocks.module.scss";
 
 const squareWidth = 10;
 const gap = 3;
-const maxX = squareWidth + gap;
 const colors = ["#51D6FF", "#8D9EC6", "#A06B9A"];
 const size = 2 * squareWidth + gap;
 
@@ -12,26 +11,18 @@ export const LoadingBlocks: React.FC = () => (
     viewBox={`0 0 ${size} ${size}`}
     xmlns="http://www.w3.org/2000/svg"
     width="100%"
+    className={styles.svg}
   >
     {colors.map((color, i) => (
-      <motion.rect
+      <rect
         key={i}
-        x={0}
-        y={0}
+        cx={squareWidth / 2}
+        cy={squareWidth / 2}
         width={squareWidth}
         height={squareWidth}
         fill={color}
         rx={squareWidth / 10}
-        animate={{
-          x: [0, maxX, maxX, 0, 0],
-          y: [0, 0, maxX, maxX, 0],
-        }}
-        transition={{
-          times: [0, 0.25, 0.5, 0.75, 1],
-          repeat: Infinity,
-          duration: 2,
-          delay: i * 0.65,
-        }}
+        className={styles.rect}
       />
     ))}
   </svg>
--- src/components/Samples/LoadingSpinner.tsx
+++ src/components/Samples/LoadingSpinner.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
-import { motion } from "framer-motion";
+import styles from "./LoadingSpinner.module.scss";
 
 const edgeScale = 1.15;
 const trackRadius = 50;
 const dotRadius = 7;
 const numDots = 7;
 const numDivisions = 18;
+const PI = Math.PI;
 const dotArray = Array.from({ length: numDots }).map((_, i) => i);
 
 export const LoadingSpinner: React.FC<{ color?: string }> = ({
@@ -18,30 +19,20 @@ export const LoadingSpinner: React.FC<{ color?: string }> = ({
         2 * edgeScale * trackRadius
       } ${2 * edgeScale * trackRadius}`}
       width="100%"
+      className={styles.spinner}
     >
       {dotArray.map((i) => {
         const cx = trackRadius * Math.cos((i * 2 * Math.PI) / numDivisions);
         const cy = -trackRadius * Math.sin((i * 2 * Math.PI) / numDivisions);
         return (
-          <motion.circle
+          <circle
+            v-for="i in numDots"
             key={i}
             cx={cx}
             cy={cy}
             r={dotRadius}
             fill={color}
-            style={{
-              originX: `0px`,
-              originY: `0px`,
-            }}
-            animate={{
-              rotate: [0, 360, 360],
-            }}
-            transition={{
-              times: [0, 0.85, 1],
-              repeat: Infinity,
-              duration: 2,
-              delay: 0.063 * i,
-            }}
+            className={styles.circle}
           />
         );
       })}
